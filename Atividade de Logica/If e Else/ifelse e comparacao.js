49fb53542f4c001d280af6ad434aae061fbca03f
Matheus Felipe <marcos.junior357@hotmail.com>
2026-02-01 19:23:55 -0300

    If e else/Comparação

---
+++ Atividade de Logica/If e Else/ifelse e comparacao.js
@@ -0,0 +1,17 @@
+let ano = "2000"; // Se o numero estive dentro de aspa esse numero é uma String e não um Inteiro.
+
+if(ano===2000){
+    console.log("Isso é um numero inteiro!")
+}else {
+    console.log("Isso não é um numero inteiro!")
+}
+
+let idade = 25;
+
+idade = "25"
+
+if(idade !== 25){
+    console.log("O valor ou o tipo da idade NÃO está correto!")
+} else {
+    console.log("O valor e o tipo da idade estão corretos!")
+}
