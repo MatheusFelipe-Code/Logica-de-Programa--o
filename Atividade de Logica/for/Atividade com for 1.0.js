126da0a66ca36de0fcd99a6f29988a97ad0e8a65
Matheus Felipe <marcos.junior357@hotmail.com>
2026-02-02 20:53:27 -0300

    Atividade com o For

---
+++ Atividade de Logica/for/Atividade com for 1.0.js
@@ -0,0 +1,17 @@
+
+let numero = 20;
+
+for (let i = 0; i <=numero; i++) {
+    console.log(`O Numero é ${i}`)
+}
+
+const nomes = ['Matheus', 'Felipe', 'Lucas', 'Pedro'];
+
+for (let i = 0; i <nomes.length; i++){
+    console.log(`Indice ${i}`, nomes[i]);
+}
+
+let tabuada = 5;
+for (let i = 0; i <= 10; i++){
+    console.log([tabuada * i])
+}
